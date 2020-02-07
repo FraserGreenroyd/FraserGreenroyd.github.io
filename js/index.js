4075d4fc3ba7610951897be5b79282e84d6e2db6
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2020-02-07 09:58:34 +0000

    Add namespace object view

--- js/index.js
+++ js/index.js
@@ -3,8 +3,14 @@ app.controller('indexController', function($scope, $window, $http, $filter, noti
 	$scope.isLoading = true;
 
 	$scope.currentObject = {};
+	$scope.selectedNamespaceObjects = [];
+	$scope.selectedNamespace = "";
 
 	$scope.objects = [];
+	$scope.methods = [];
+
+	$scope.namespaces = [];
+	$scope.displayNamespace = false;
 
 	$scope.handleFailure = function(response)
 	{
@@ -18,6 +24,10 @@ app.controller('indexController', function($scope, $window, $http, $filter, noti
 		var namespace = $location.search().namespace;
 		var object = $location.search().object;
 
+		$scope.displayNamespace = false;
+
+		$scope.selectedNamespace = namespace;
+
 		$http.get('js/methods.json').then(function(response) {
 			$scope.methods = response.data;
 
@@ -25,37 +35,54 @@ app.controller('indexController', function($scope, $window, $http, $filter, noti
 				$scope.objects = response.data;
 				$scope.currentObject = null;
 
-				$scope.objects.filter(function(obj) {
-					if(obj.namespace == namespace && obj.memberName == object)
-						$scope.currentObject = obj;
+				$scope.objects.forEach(function(obj) {
+					if($scope.namespaces.indexOf(obj.namespace) == -1)
+						$scope.namespaces.push(obj.namespace);
 				});
 
-				if($scope.currentObject != null)
+				if(object != null && object != undefined)
 				{
-					var types = [];
-					types.push(namespace + "." + object);
-					$scope.currentObject.inheritance.forEach(function(obj) {
-						types.push(obj.namespace + "." + obj.memberName);
+					$scope.objects.filter(function(obj) {
+						if(obj.namespace == namespace && obj.memberName == object)
+							$scope.currentObject = obj;
 					});
 
-					var methods = [];
-					$scope.methods.filter(function(obj) {
-						obj.inputs.filter(function(input) {
-							if(types.indexOf(input.namespace + "." + input.memberName) != -1)
-							{
-								if(methods.indexOf(obj) == -1)
-									methods.push(obj);
-							}
+					if($scope.currentObject != null)
+					{
+						var types = [];
+						types.push(namespace + "." + object);
+						$scope.currentObject.inheritance.forEach(function(obj) {
+							types.push(obj.namespace + "." + obj.memberName);
 						});
+
+						var methods = [];
+						$scope.methods.filter(function(obj) {
+							obj.inputs.filter(function(input) {
+								if(types.indexOf(input.namespace + "." + input.memberName) != -1)
+								{
+									if(methods.indexOf(obj) == -1)
+										methods.push(obj);
+								}
+							});
+						});
+
+						methods.sort(function(a, b) {
+							if(a.namespace < b.namespace) return -1;
+							if(a.namespace > b.namespace) return 1;
+							return 0;
+						});
+
+						$scope.currentObject.methods = methods;
+					}
+				}
+				else
+				{
+					$scope.objects.filter(function(obj) {
+						if(obj.namespace == namespace)
+							$scope.selectedNamespace.push(obj);
 					});
 
-					methods.sort(function(a, b) {
-						if(a.namespace < b.namespace) return -1;
-						if(a.namespace > b.namespace) return 1;
-						return 0;
-					});
-
-					$scope.currentObject.methods = methods;
+					$scope.displayNamespace = true;
 				}
 
 				$scope.isLoading = false;		
@@ -77,5 +104,9 @@ app.controller('indexController', function($scope, $window, $http, $filter, noti
 	$scope.goToObject = function(object) {
 		$location.search('namespace', object.namespace);
 		$location.search('object', object.memberName);
+	};
+
+	$scope.goToNamespace = function(namespace) {
+		$location.search('namespace', namespace.namespace);
 	};
 });
